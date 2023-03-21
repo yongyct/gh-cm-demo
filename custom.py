3da389d99cefdb578150bb63f2fa5d901dd3a5d8
yongyct <yongyct@gmail.com>
2023-03-21 18:40:21 +0800

    initial commit

---
+++ custom.py
@@ -0,0 +1,16 @@
+#import io
+#import json
+import pandas as pd
+
+
+def transform(data, model):
+   data = data.fillna(0)
+   return data
+
+#def read_input_data(input_binary_data):
+#   print(type(input_binary_data))
+#   print(json.loads(io.BytesIO(input_binary_data).read()))
+#   data = pd.read_json(io.BytesIO(input_binary_data))
+#   return data
+
+# gh cm test 1
